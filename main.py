840f5a189590e34c7f8b80c1c41033d7b3519624
jatinsajwan3841 <58560850+jatinsajwan3841@users.noreply.github.com>
2020-07-24 16:14:51 +0530

    added comments

--- main.py
+++ main.py
@@ -13,14 +13,22 @@ Interval=int(input("Time interval in between subsequent messages"))
 def whatsappspam():
     # for using the default chrome profile
     options = webdriver.ChromeOptions()
+    
+    #generally location of default profile, change the username according to your system username
     options.add_argument("user-data-dir=C:\\Users\\Jatin\\AppData\\Local\\Google\\Chrome\\User Data")
+    
+    #change location to downloaded location of driver
     driver = webdriver.Chrome( executable_path="C:\\Users\\Jatin\\Downloads\\Compressed\\chromedriver_win32\\chromedriver.exe", options=options)
+    
+    #for separate instance remove above code and remove hash from below code.
+    #driver = webdriver.Chrome( executable_path="downloaded driver location")
 
     #Opening WhatsappWeb
     driver.get('https://web.whatsapp.com')
 
+    #for loading time wait.
     time.sleep(15)
-    elem = driver.find_element_by_xpath('//*[@id="side"]/div[1]/div/label/div/div[2]')
+    elem = driver.find_element_by_xpath('//*[@id="side"]/div[1]/div/label/div/div[2]') #finding search bar
     elem.send_keys(Name)
     time.sleep(1)
     elem.send_keys(Keys.RETURN)
